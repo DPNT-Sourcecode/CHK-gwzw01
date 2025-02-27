4a2dfdcaa021efce70be4a5985129b2e1ef28ef8
sbx_user1051 <sbx_user1051@169.254.7.113>
2025-02-27 18:50:48 +0000

    Thu Feb 27 18:50:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # we presume that the input string is a sequence of characters,
+    # where each character is a valid SKU.
+
+    # Let's start by validating the input.
+    
+
